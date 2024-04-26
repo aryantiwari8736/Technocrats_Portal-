85a530920296d01eccf0fe4abd04ba8ccba386da
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-04-27 00:01:38 +0530

    Initial Setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("A web Portal");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const connectDb = require("./config/db.js");
+const userRoutes=require("./routes/userRoutes.js");
+const cors=require("cors");
+
+dotenv.config();
+connectDb();
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, console.log(`Server is listening at Port:-${PORT}`));
